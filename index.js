66f2f37c4c7467b00556d055d4fe43a5fac16b3f
tusharXO <iamtushar2004@gmail.com>
2023-05-03 18:45:54 +0530

    big update

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const computerchoiceDisplay = document.getElementById("computer-choice")
 const userchoiceDisplay = document.getElementById("user-choice")
 const resultDisplay = document.getElementById("result")
-const possiblechoice = document.querySelectorAll("button")
+const possiblechoice = document.querySelectorAll("img")
 
+var images_all= ["paper.png", "rock.png","scissors.png"]
+
+let computerimage
 let userchoice
 let computerchoice
 let result
 
+var i
+
 possiblechoice.forEach(possiblechoice => possiblechoice.addEventListener('click', (e) => {
     userchoice = e.target.id
-    userchoiceDisplay.innerHTML = userchoice
+    imgselect() 
+    userchoiceDisplay.src = images_all[i]
     generatecomputerchoice()
     theresult() 
 }))
@@ -18,39 +24,54 @@ function generatecomputerchoice(){
     const randomNumber = Math.floor(Math.random()*possiblechoice.length) + 1
 
     if(randomNumber === 1){
-        computerchoice = "rock"
-    }
-    else if(randomNumber === 2){
+        computerimage = images_all[0]
         computerchoice = "paper"
     }
+    else if(randomNumber === 2){
+        computerimage = images_all[1]
+        computerchoice = "rock"
+    }
     else{
-        computerchoice = "scissor"
+        computerimage = images_all[2]
+        computerchoice = "scissors"
     }
 
-    computerchoiceDisplay.innerHTML = computerchoice
+    computerchoiceDisplay.src = computerimage
 }
 
 function theresult(){
     if(computerchoice === userchoice){
-        result = "its a draw"
+        result = "Draw"
     }
     else if(computerchoice === "rock" && userchoice === "paper"){
-        result = "you win"
+        result = "🙂"
     }
     else if(computerchoice === "paper" && userchoice === "rock"){
-        result = "you lose"
+        result = "😔"
     }
     else if(computerchoice === "rock" && userchoice === "scissor"){
-        result = "you lose"
+        result = "😔"
     }
     else if(computerchoice === "scissor" && userchoice === "rock"){
-        result = "you win"
+        result = "🙂"
     }
     else if(computerchoice === "paper" && userchoice === "scissor"){
-        result = "you win"
+        result = "🙂"
     }
     else if(computerchoice === "scissor" && userchoice === "paper"){
-        result = "you lose"
+        result = "😔"
     }
     resultDisplay.innerHTML = result
+}
+
+function imgselect(){
+    if(userchoice === "paper"){
+        i = 0
+    }
+    else if(userchoice === "scissors"){
+        i = 2
+    }
+    else{
+        i = 1
+    }
 }
